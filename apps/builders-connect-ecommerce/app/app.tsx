68e05860656e56d9f71ae20406628632acd47219
aosenit <creativitytraz22@gmail.com>
2025-03-25 11:05:52 +0100

    Initial commit

---
+++ apps/builders-connect-ecommerce/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="builders-connect-ecommerce" />
+    </div>
+  );
+}
+
+export default App;
