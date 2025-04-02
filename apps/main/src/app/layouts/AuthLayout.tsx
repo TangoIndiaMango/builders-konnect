559b3de0b1e2a1cfdcf66dc172531d256162a3ba
aosenit <creativitytraz22@gmail.com>
2025-04-02 13:13:09 +0100

    feat:setup axios, tanstack and jotai

--- apps/main/src/app/layouts/AuthLayout.tsx
+++ apps/main/src/app/layouts/AuthLayout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
-import { authBackground } from '../utils/assets/background';
+import { authBackground } from '../lib/assets/background';
 
 interface AuthLayoutProps {
   children?: React.ReactNode;
