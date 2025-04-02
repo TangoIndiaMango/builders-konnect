9aabd108ba9c77cb1e96d5bd8ceb74a852def3e9
aosenit <creativitytraz22@gmail.com>
2025-04-02 10:09:03 +0100

    feat:update create account to create password and add vendor to route

--- apps/main/src/app/app.tsx
+++ apps/main/src/app/app.tsx
@@ -3,21 +3,19 @@ import RegisterVendor from './pages/auth/register-vendor';
 import AuthOutlet from './pages/auth/auth-outlets';
 import Login from './pages/auth/login';
 import ForgotPassword from './pages/auth/forgot-password';
-import CreateVendorAccount from './pages/auth/create-vendor-account';
+import CreatePassword from './pages/auth/create-vendor-account';
 
 const App = () => {
-
-return (
-  <Routes>
-
-    <Route path="auth" element={<AuthOutlet />}>
-      <Route path="" element={<RegisterVendor />} />
-      <Route path="create-vendor" element={<CreateVendorAccount />} />
-      <Route path="login" element={<Login />} />
-      <Route path="forgot-password" element={<ForgotPassword />} />
-    </Route>
-  </Routes>
-)
-}
+  return (
+    <Routes>
+      <Route path="vendor/auth" element={<AuthOutlet />}>
+        <Route path="" element={<RegisterVendor />} />
+        <Route path="create-password" element={<CreatePassword />} />
+        <Route path="login" element={<Login />} />
+        <Route path="forgot-password" element={<ForgotPassword />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
--- apps/main/src/app/layouts/AuthLayout.tsx
+++ apps/main/src/app/layouts/AuthLayout.tsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import { Space, Steps } from 'antd'
-import Header from '../components/Header'
-import { authBackground } from '../utils/assets/background'
+import React from 'react';
+
+import Header from '../components/Header';
+import { authBackground } from '../utils/assets/background';
 
 interface AuthLayoutProps {
   children?: React.ReactNode;
 }
 
 const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
-
-
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
@@ -20,13 +18,10 @@ const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
           backgroundImage: `linear-gradient(to bottom, rgba(39, 86, 214, 0.3), rgba(0, 51, 153, 0.3)), url(${authBackground})`,
         }}
       >
-          <div className="">
-            {children}
-          </div>
-
+        <div className="">{children}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
