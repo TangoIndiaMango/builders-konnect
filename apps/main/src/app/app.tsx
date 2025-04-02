4f5b24fc695b2a1db0c1b37dfb16cee8943e6516
aosenit <creativitytraz22@gmail.com>
2025-04-02 13:16:03 +0100

    feat:merge fixes

--- apps/main/src/app/app.tsx
+++ apps/main/src/app/app.tsx
@@ -3,7 +3,6 @@ import RegisterVendor from './pages/auth/register-vendor';
 import AuthOutlet from './pages/auth/auth-outlets';
 import Login from './pages/auth/login';
 import ForgotPassword from './pages/auth/forgot-password';
-import CreateVendorAccount from './pages/auth/create-vendor-account';
 import ChangePassword from './pages/auth/change-password';
 import MultipleAccounts from './pages/auth/multiple-accounts';
 import CreatePassword from './pages/auth/create-vendor-account';
@@ -14,7 +13,6 @@ const App = () => {
       <Route path="auth/vendor" element={<AuthOutlet />}>
         <Route path="register-vendor" element={<RegisterVendor />} />
         <Route path="create-password" element={<CreatePassword />} />
-        <Route path="create-vendor" element={<CreateVendorAccount />} />
         <Route path="login" element={<Login />} />
         <Route path="forgot-password" element={<ForgotPassword />} />
         <Route path="change-password" element={<ChangePassword />} />
