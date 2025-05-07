d0c1743c73aa0f9544b6031184862d31619e3b13
aosenit <creativitytraz22@gmail.com>
2025-05-07 14:25:19 +0100

    feat:build fix

--- apps/main/src/main.tsx
+++ apps/main/src/main.tsx
@@ -1,5 +1,4 @@
 import * as ReactDOM from 'react-dom/client';
-import '@ant-design/v5-patch-for-react-19';
 import App from './app/app';
 import { ConfigProvider, App as AntApp } from 'antd';
 import { BrowserRouter as Router } from 'react-router-dom';
